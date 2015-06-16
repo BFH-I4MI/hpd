aebf36d4f62114dc41bd9c6a293bddea72cb97f3
Kevin Tippenhauer <kevin.tippenhauer@bfh.ch>
2015-06-16 10:56:57 +0200

    Included interceptors in the project

---
+++ src/main/java/ch/bfh/i4mi/interceptor/AttributeConditionsInterceptor.java
@@ -0,0 +1,257 @@
+package ch.bfh.i4mi.interceptor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.directory.api.ldap.model.entry.Attribute;
+import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.entry.Modification;
+import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.server.core.api.DirectoryService;
+import org.apache.directory.server.core.api.interceptor.BaseInterceptor;
+import org.apache.directory.server.core.api.interceptor.context.AddOperationContext;
+import org.apache.directory.server.core.api.interceptor.context.ModifyOperationContext;
+import org.apache.directory.server.core.api.interceptor.context.OperationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AttributeConditionsInterceptor extends BaseInterceptor {
+
+	Logger LOG = LoggerFactory.getLogger(AttributeConditionsInterceptor.class);
+
+	private final static String OU_HEALTH_ORG = "ou=HCRegulatedOrganization";
+	private final static String OU_HEALTH_PRO = "ou=HCProfessional";
+	private final static Boolean SINGLE = true;
+	private final static Boolean MULTIPLE = false;
+	private final static Boolean OPTIONAL = true;
+	private final static Boolean REQUIRED = false;
+
+	/**
+	 * Contains the organizational attributes which are different from the IHE
+	 * standard.
+	 */
+	private List<HPDAttributeSettings> orgAttrToCheck = new ArrayList<HPDAttributeSettings>();
+
+	/**
+	 * Contains the health professional attributes which are different from the
+	 * IHE standard.
+	 */
+	private List<HPDAttributeSettings> proAttrToCheck = new ArrayList<HPDAttributeSettings>();
+
+	/** The OperationContext. */
+	private OperationContext operationContext;
+
+	public final void init(final DirectoryService aDirectoryService)
+			throws LdapException {
+		LOG.debug("Initialiazing the AttributeConditionsInterceptor");
+
+		// name, isSingle, isOptional, minOccurence, maxOccurence
+		// RELATIONSHIP ATTRIBUTES ARE CHECKED FROM THE RELATIONSHIP INTERCEPTOR
+
+		try {
+			// HcIdentifier geht nicht null ist möglich
+			orgAttrToCheck.add(new HPDAttributeSettings("HcIdentifier", SINGLE,
+					REQUIRED, 1, 1));
+
+			orgAttrToCheck.add(new HPDAttributeSettings("o", MULTIPLE, REQUIRED, 1,
+					Integer.MAX_VALUE));
+			orgAttrToCheck.add(new HPDAttributeSettings("businessCategory", MULTIPLE,
+					REQUIRED, 1, Integer.MAX_VALUE));
+			orgAttrToCheck.add(new HPDAttributeSettings("hpdProviderPracticeAddress",
+					MULTIPLE, OPTIONAL, 0, 2));
+
+			
+			// Professionals are tested and working.
+			proAttrToCheck.add(new HPDAttributeSettings("HcIdentifier", SINGLE,
+					REQUIRED, 1, 1));
+			proAttrToCheck.add(new HPDAttributeSettings("hpdProviderPracticeAddress",
+					SINGLE, OPTIONAL, 0, 1));
+			proAttrToCheck.add(new HPDAttributeSettings("givenName", MULTIPLE,
+					REQUIRED, 1, Integer.MAX_VALUE));
+			proAttrToCheck.add(new HPDAttributeSettings("sn", SINGLE, REQUIRED, 1, 1));
+		} catch (Exception e) {
+			LOG.debug(e.getMessage());
+		}
+		LOG.debug("AttributeConditionsInterceptor - End init()");
+		super.init(aDirectoryService);
+	}
+
+	@Override
+	public final void add(final AddOperationContext addOperationContext)
+			throws LdapException {
+		this.operationContext = addOperationContext;
+		Entry entry = operationContext.getEntry();
+
+		LOG.debug(">> OptionalRequiredOperation : AddContext for Dn '"
+				+ entry.getDn() + "'");
+
+		if (isEntryOfOU(entry, OU_HEALTH_ORG)) {
+			checkAddOp(orgAttrToCheck);
+		} else if (isEntryOfOU(entry, OU_HEALTH_PRO)) {
+			LOG.debug("Add Professional");
+			checkAddOp(proAttrToCheck);
+		}
+
+		LOG.debug("<< OptionalRequiredOperation : successful");
+		next(addOperationContext);
+	}
+
+	@Override
+	public final void modify(final ModifyOperationContext modifyOperationContext)
+			throws LdapException {
+		List<Modification> items = modifyOperationContext.getModItems();
+		this.operationContext = modifyOperationContext;
+		Entry entry = operationContext.getEntry();
+
+		
+
+		LOG.debug(">> OptionalRequiredOperation : ModifyContext for Dn '"
+				+ entry.getDn() + "'");
+
+		for (Modification modification : items) {
+			if (isEntryOfOU(entry, OU_HEALTH_ORG)) {
+				checkModOp(modification, orgAttrToCheck);
+			} else if (isEntryOfOU(entry, OU_HEALTH_PRO)) {
+				checkModOp(modification, proAttrToCheck);
+			}
+		}
+		LOG.debug("<< OptionalRequiredOperation : successful");
+		next(modifyOperationContext);
+	}
+
+	private void checkAddOp(List<HPDAttributeSettings> attributesToCheck)
+			throws LdapException {
+		Entry entry = operationContext.getEntry();
+		for (HPDAttributeSettings attributeToCheck : attributesToCheck) {
+
+			Attribute attribute = entry
+					.get(attributeToCheck.getAttributeName());
+
+			// Check if the attribute is required and present otherwise throw
+			// exception.
+			if (!attributeToCheck.isOptional() && attribute == null) {
+				throw new LdapException("Required attribute '"
+						+ attributeToCheck.getAttributeName() + "' is null.");
+			} else if (attributeToCheck.isSingleValue()) {
+				if (attribute != null
+						&& entry.get(attributeToCheck.getAttributeName())
+								.size() != 1) {
+					// If attribute is null it must be optional.
+					throw new LdapException("Single attribute '"
+							+ attributeToCheck.getAttributeName()
+							+ "' occures more than once.");
+				}
+			} else {
+				// Must be a multiple value attribute.
+
+				// Check if it fulfills the minimal number requirement.
+				if (entry.get(attributeToCheck.getAttributeName()).size() < attributeToCheck
+						.getNumberOfMinOccurence()) {
+
+					throw new LdapException(
+							"Attribute '"
+									+ attributeToCheck.getAttributeName()
+									+ "' doesn't fulfill it's minimal number requirement of "
+									+ attributeToCheck
+											.getNumberOfMinOccurence() + ".");
+					// Or the maximal number requirement.
+				} else if (entry.get(attributeToCheck.getAttributeName())
+						.size() > attributeToCheck.getNumberOfMaxOccurence()) {
+
+					throw new LdapException(
+							"Attribute '"
+									+ attributeToCheck.getAttributeName()
+									+ "' doesn't fulfill it's maximal number requirement of "
+									+ attributeToCheck
+											.getNumberOfMinOccurence() + ".");
+
+				}
+			}
+		}
+	}
+
+	private void checkModOp(Modification modification,
+			List<HPDAttributeSettings> attributesToCheck) throws LdapException {
+		Attribute attribute = modification.getAttribute();
+		for (HPDAttributeSettings attributeToCheck : attributesToCheck) {
+			if (attribute.getUpId().equalsIgnoreCase(
+					attributeToCheck.getAttributeName())) {
+				switch (modification.getOperation()) {
+				case ADD_ATTRIBUTE:
+					checkAddMod(attribute, attributeToCheck);
+					break;
+				case REPLACE_ATTRIBUTE:
+					checkReplaceMod(attribute, attributeToCheck);
+					break;
+				case REMOVE_ATTRIBUTE:
+					checkRemoveMod(attribute, attributeToCheck);
+					break;
+				}
+			}
+		}
+	}
+
+	private void checkRemoveMod(Attribute attribute, HPDAttributeSettings attrProperties)
+			throws LdapException {
+		if (!attrProperties.isOptional()) {
+			Entry entry = this.operationContext.getEntry();
+			// Required, if size equals 1 throw exception
+			if (entry.get(attribute.getUpId()).size() == attrProperties
+					.getNumberOfMinOccurence()) {
+				throw new LdapException(
+						"ModifyRemove operation on an attribute which has already "
+								+ "reached it's minimum number.");
+			}
+		}
+	}
+
+	private void checkReplaceMod(Attribute attribute,
+			HPDAttributeSettings attrProperties) throws LdapException {
+		Entry entry = this.operationContext.getEntry();
+		if (entry.get(attribute.getUpId()).size() != 1) {
+			throw new LdapException(
+					"ModifyReplace operation on an attribute which doesn't exist "
+							+ "or has multiple attribute values.");
+		}
+	}
+
+	private void checkAddMod(Attribute attribute, HPDAttributeSettings attrProperties)
+			throws LdapException {
+		// Optional Single kein Attibute -> OK
+		// Optional Multi kein Attribute -> OK
+		// Optional Single mit Attribute -> FALSE
+		// Optional Multi mit Attribute -> anz. Attribute < max -> OK sonst
+		// FALSE
+		// Required Single kein Attribute -> sollte es nicht geben
+		// Required Multi kein Attribute -> sollte es nicht geben
+		// Required Single mit Attribute -> Fehler REPLACE verwenden
+		// Required Multi mit Attributeen -> anz. Attribute < max -> OK sonst
+		// FALSE
+		Entry entry = this.operationContext.getEntry();
+		if (attrProperties.isSingleValue()) {
+			if (entry.get(attribute.getUpId()) != null) {
+				// There is already one attribute of this type, throw exception
+				// because it's single value
+				throw new LdapException(
+						"ModifyAdd Ooperation on an existing single "
+								+ "attribute value.");
+			}
+		} else {
+			// Multiple
+			if (entry.get(attribute.getUpId()).size() == attrProperties
+					.getNumberOfMaxOccurence()) {
+				throw new LdapException(
+						"ModifyAdd operation on an attribute which has reached it's "
+								+ "maximal occurence.");
+			}
+		}
+	}
+
+	private static boolean isEntryOfOU(final Entry entry, final String rdnName) {
+		if (entry.getDn().getRdn(1).getName().equals(rdnName)) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ src/main/java/ch/bfh/i4mi/interceptor/RelationshipChecker.java
@@ -0,0 +1,281 @@
+package ch.bfh.i4mi.interceptor;
+
+import org.apache.directory.api.ldap.model.entry.Attribute;
+import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.entry.Value;
+import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.exception.LdapInvalidAttributeValueException;
+import org.apache.directory.api.ldap.model.exception.LdapInvalidDnException;
+import org.apache.directory.api.ldap.model.name.Dn;
+import org.apache.directory.server.core.api.entry.ClonedServerEntry;
+import org.apache.directory.server.core.api.interceptor.context.OperationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RelationshipChecker {
+	/**
+	 * Name of the attribute to check.
+	 */
+	private String ownerAttributeName;
+
+	private String memberOfAttributeName;
+
+	private String catAttrName;
+
+	private String catValueForCommunity;
+
+	private String ouHealthOrgRdn;
+
+	private String ouHealthProRdn;
+
+	private OperationContext opContext;
+	private Entry opContextEntry;
+	private Attribute attribute;
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(RelationshipInterceptor.class);
+
+	RelationshipChecker(String anOwnerAttributeName,
+			String aMemberOfAttributeName, String aCatAttrName,
+			String aCatValueForCommunity, String anOuHealthOrgRdn,
+			String ouHeathProRdn, OperationContext anOperationContext,
+			Entry anOpContextEntry, Attribute anAttribute) throws LdapException {
+
+			this.ownerAttributeName = anOwnerAttributeName;
+			this.memberOfAttributeName = aMemberOfAttributeName;
+			this.catAttrName = aCatAttrName;
+			this.catValueForCommunity = aCatValueForCommunity;
+			this.ouHealthOrgRdn = anOuHealthOrgRdn;
+			this.ouHealthProRdn = ouHeathProRdn;
+			this.opContext = anOperationContext;
+			this.opContextEntry = anOpContextEntry;
+			this.attribute = anAttribute;
+	}
+
+	protected void checkAddRequest() throws LdapException {
+		if (this.opContextEntry.get(this.memberOfAttributeName).size() > 1) {
+			// Multiple memberOf values
+
+			if (isEntryOfOU(this.opContextEntry, this.ouHealthOrgRdn)) {
+				// HO and multiple memberOf so it must be a Root-HO
+				for (Value<?> value : this.attribute) {
+					Entry ownerOfRelationship = getOwnerOfRelationship(value);
+
+					if (!isCommunity(ownerOfRelationship)) {
+						throw new LdapException(
+								"Root-HO must not contain any non community relationships. "
+										+ "Sub-HOs are single value.");
+					}
+				}
+
+			} else if (isEntryOfOU(this.opContextEntry, this.ouHealthProRdn)) {
+				// HP
+				for (Value<?> value : this.attribute) {
+					Entry ownerOfRelationship = getOwnerOfRelationship(value);
+
+					if (!isEntryOfOU(ownerOfRelationship, this.ouHealthOrgRdn)) {
+						throw new LdapException(
+								"HPs can only be connected to organizations or a community.");
+					} else if (isEntryOfOU(ownerOfRelationship,
+							this.ouHealthOrgRdn)
+							&& isCommunity(ownerOfRelationship)) {
+						// Owner is a community not a HO and not the only value.
+						throw new LdapException(
+								"Community relationship for HPs is only allowed when there are no "
+										+ "other relationships.");
+					}
+				}
+			} else {
+				throw new LdapException(
+						"Unexpected entry with multiple memberOf values.");
+			}
+		} else {
+			// Single memberOf value
+			Entry ownerOfRelationship = getOwnerOfRelationship(this.attribute
+					.get());
+			if (isEntryOfOU(this.opContextEntry, this.ouHealthOrgRdn)) {
+				if (!isEntryOfOU(ownerOfRelationship, this.ouHealthOrgRdn)) {
+					// Entry is connected neither to a HO nor a community.
+					throw new LdapException(
+							"HOs must be connected with a HO or communities.");
+				}
+
+			} else if (isEntryOfOU(this.opContextEntry, this.ouHealthProRdn)) {
+				if (!isEntryOfOU(ownerOfRelationship, this.ouHealthOrgRdn)) {
+					// Entry is connected neither to a HO nor a community.
+					throw new LdapException(
+							"HOs must be connected with a HO or communities.");
+				}
+			} else {
+				throw new LdapException(
+						"Unexpected entry with a single memberOf values.");
+			}
+		}
+	}
+
+	protected void checkModifyAddRequest() throws LdapException {
+		LOG.debug("checkModifyAddRequest()");
+		if (this.opContextEntry.get(this.memberOfAttributeName) == null
+				|| this.opContextEntry.get(this.memberOfAttributeName).size() < 1) {
+			LOG.debug("size() < 1");
+			// Single memberOf value
+			Entry ownerOfRelationship = getOwnerOfRelationship(this.attribute
+					.get());
+			if (isEntryOfOU(this.opContextEntry, this.ouHealthOrgRdn)) {
+				if (!isEntryOfOU(ownerOfRelationship, this.ouHealthOrgRdn)) {
+					// Entry is connected neither to a HO nor a
+					// community.
+					throw new LdapException(
+							"HOs must be connected with a HO or communities.");
+				}
+			} else if (isEntryOfOU(this.opContextEntry, this.ouHealthProRdn)) {
+				if (!isEntryOfOU(ownerOfRelationship, this.ouHealthOrgRdn)) {
+					// Entry is connected neither to a HO nor a
+					// community.
+					throw new LdapException(
+							"HOs must be connected with a HO or communities.");
+				}
+			} else {
+				throw new LdapException(
+						"Unexpected entry with a single memberOf values.");
+			}
+		} else if (this.opContextEntry.get(this.memberOfAttributeName).size() == 1) {
+			LOG.debug("size() == 1");
+			// 1. Check type of the opContextEntry
+			Entry ownerOfExistingRelationship = getOwnerOfRelationship(this.opContextEntry
+					.get(this.memberOfAttributeName).get());
+
+			LOG.debug("Owner of current R: "
+					+ ownerOfExistingRelationship.toString());
+			if (isCommunity(ownerOfExistingRelationship)) {
+				LOG.debug("Owner is Community");
+
+				// relationship with a community
+				Entry ownerOfNewRelationship = getOwnerOfRelationship(this.attribute
+						.get());
+				if (isEntryOfOU(this.opContextEntry, this.ouHealthOrgRdn)) {
+					if (!isCommunity(ownerOfNewRelationship)) {
+						throw new LdapException(
+								"Root-HO must not contain any non community relationships.");
+					}
+				} else if (isEntryOfOU(this.opContextEntry, this.ouHealthProRdn)) {
+					throw new LdapException(
+							"Community relationship for HPs is only allowed when there are no "
+									+ "other relationships.");
+				}
+			} else if (isEntryOfOU(this.opContextEntry, this.ouHealthOrgRdn)) {
+				LOG.debug("isEntryofOU");
+				// relationship is a HO
+				throw new LdapException(
+						"Sub-HO can only have one relationship to another Sub-HO or Root-HO");
+			} else if (isEntryOfOU(this.opContextEntry, this.ouHealthProRdn)) {
+				if (isCommunity(getOwnerOfRelationship(this.attribute.get()))) {
+					// The old relationship is to an HO, the new is to a
+					// community
+					throw new LdapException(
+							"Community relationship for HPs is only allowed when there are no "
+									+ "other relationships.");
+				}
+			}
+		} else {
+			// if (this.opContextEntry.get(this.memberOfAttributeName).size() >
+			// 1)
+			// Is already a multiple
+			LOG.debug("size() > 1");
+			Entry ownerOfRelationship = getOwnerOfRelationship(this.attribute
+					.get());
+
+			if (isEntryOfOU(this.opContextEntry, this.ouHealthOrgRdn)) {
+				// Is a Root-HO
+				LOG.debug("Is a Root-HO");
+				if (!isCommunity(ownerOfRelationship)) {
+					throw new LdapException(
+							"Root-HO must not contain any non community relationships.");
+				}
+
+			} else if (isEntryOfOU(this.opContextEntry, this.ouHealthProRdn)) {
+				// Is a HPProfessional
+				LOG.debug("Is a HPProfessional");
+				if (isCommunity(ownerOfRelationship)) {
+					throw new LdapException(
+							"Community relationship for HPs is only allowed when there are no "
+									+ "other relationships.");
+				}
+			}
+		}
+
+	}
+
+	protected void checkModifyReplace() throws LdapException {
+		if (this.opContextEntry.get(this.memberOfAttributeName).size() != 1) {
+			throw new LdapException(
+					"RelationshipInterceptor.ReplaceWrongNoOfAttr");
+		}
+		// Single memberOf value
+		Entry ownerOfRelationship = getOwnerOfRelationship(this.attribute.get());
+		if (isEntryOfOU(this.opContextEntry, this.ouHealthOrgRdn)) {
+			if (!isEntryOfOU(ownerOfRelationship, this.ouHealthOrgRdn)) {
+				// Entry is connected neither to a HO nor a
+				// community.
+				throw new LdapException(
+						"HOs must be connected with a HO or communities.");
+			}
+
+		} else if (isEntryOfOU(this.opContextEntry, this.ouHealthProRdn)) {
+			if (!isEntryOfOU(ownerOfRelationship, this.ouHealthOrgRdn)) {
+				// Entry is connected neither to a HO nor a
+				// community.
+				throw new LdapException(
+						"HOs must be connected with a HO or communities.");
+			}
+		} else {
+			throw new LdapException(
+					"Unexpected entry with a single memberOf values.");
+		}
+	}
+
+	private static boolean isEntryOfOU(final Entry entry, final String rdnName) {
+		if (entry.getDn().getRdn(1).getName().equals(rdnName)) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isCommunity(Entry entry)
+			throws LdapInvalidAttributeValueException {
+		if (entry.get(this.catAttrName) != null
+				&& entry.get(this.catAttrName).getString()
+						.equalsIgnoreCase(this.catValueForCommunity)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Returns the owner of a relationship based on the relationship dn.
+	 * 
+	 * @param value
+	 *            Value containing the dn of the relationship.
+	 * @return the owner entry of the relationship.
+	 * @throws LdapException
+	 *             thrown on invalid value.
+	 * @throws LdapInvalidDnException
+	 *             thrown on invalid value.
+	 */
+
+	private Entry getOwnerOfRelationship(final Value<?> value)
+			throws LdapInvalidDnException, LdapException {
+		Entry relationshipEntry = ((ClonedServerEntry) this.opContext
+				.getSession().lookup(new Dn(value.getString())))
+				.getOriginalEntry();
+
+		Entry ownerOfRelationship = ((ClonedServerEntry) this.opContext
+				.getSession().lookup(
+						new Dn(relationshipEntry.get(this.ownerAttributeName)
+								.getString()))).getOriginalEntry();
+
+		return ownerOfRelationship;
+
+	}
+
+}
